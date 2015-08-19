d5219d6ad335eafa934cd6cd81abb170d30ced8d
josephh <josephh@rocketmail.com>
2015-08-19 08:33:27 +0100

    journals and entries routes working without any nesting

--- app/routes/journals.js
+++ app/routes/journals.js
@@ -1,4 +1,16 @@
 import Ember from 'ember';
 
-export default Ember.Route.extend({
+var Journal = Ember.Object.extend({
+  year: new Date(2015, 1, 1)
+});
+
+var years = [];
+for(var i=2012; i<2051; i++){
+  console.log('next i :' + i);
+  years.push(Journal.create({year: new Date(i, 1, 1)}));
+};
+
+export default Ember.Route.extend({ model: function() {
+  return years;
+}
 });
