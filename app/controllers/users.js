818eab6c7bb2974d96b0c3f96a75df61338df162
josephh <josephh@rocketmail.com>
2015-12-08 17:02:54 +0000

    removing unwanted templates and routes

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -2,17 +2,24 @@ import Ember from 'ember';
 
 var Users = Ember.Object.extend({
   // auth providers probably merit their own controller/model
-   loginProviders: [],
-   createProviders: []
+   authProviders: []
  });
 
 var providers = ['Google', 'Facebook'];
 
 var users = Users.create({
-  loginProviders: Ember.ArrayProxy.create( {content: Ember.A(providers)} ),
-  createProviders: Ember.ArrayProxy.create( {content: Ember.A(providers)} )
+  authProviders: Ember.ArrayProxy.create( {content: Ember.A(providers)} )
 });
 
 export default Ember.Controller.extend({
-  users: users
+  users: users,
+  actions: {
+    login: function(provider) {
+      console.log('login with ' + provider);
+    },
+
+    create: function(provider) {
+      console.log('create with ' + provider);
+    }
+  }
 });
