48cde4c5e84cbb40ded8901c0306bd295234b0bd
josephh <josephh@rocketmail.com>
2016-01-18 16:08:52 +0000

    tidy up docs and syntax

--- app/routes/users.js
+++ app/routes/users.js
@@ -2,74 +2,68 @@
 import Ember from 'ember';
 
 var authenticate = function(routeContext, provider, callback) {
-
-  if(provider.toLowerCase() === 'google' && false === true){  // false === true developer laziness...
+  if (provider.toLowerCase() === 'google' && false === true) {
+    console.log('<<< oauth2 implicit flow >>>');
     routeContext.get('torii')
     /**
-    * In the 'implicit' flow, the promise returned from 'open(...)' contains an
-    * access token returned google, intended for direct use inside your app.
-    * This flow can be entirely client-side: no adapter or session information
-    * is relied on (and the ember implementation demonstrates this).
-    */
+     * In the 'implicit' flow, the promise returned from 'open(...)' contains an
+     * access token returned google, intended for direct use inside your app.
+     * This flow can be entirely client-side: no adapter or session information
+     * is relied on (and the ember implementation demonstrates this).
+     */
     .open('google-oauth2-bearer')
-    .then(authData => {
-      console.log('googleAuth var = ' + authData);
-      var prop = null;
-      for (prop in authData){
-        console.log('next key = ' + prop + '; value = ' + authData[prop]);
-      }
-      var googleToken = authData.authorizationToken.access_token;  // access token
-      console.log('Google token: ' + googleToken + " >>> now what?!");
+      .then(authData => {
+        console.log('FYB client >> Authenticated with Google.');
+        var googleToken = authData.authorizationToken.access_token;
+        console.log('Google access token = ' + googleToken + ".");
 
-      /*   This bit is for interacting with the jwt provider - i.e. our server
-      routeContext.get('session')
-      .authenticate('simple-auth-authenticator:jwt', { password: googleToken} )
-      .then(callback(), function (error) {
-      console.log('custom token authentication failed!', error.message);
+        // Now hand the access token to our FYB server
+        routeContext.get('session')
+          .authenticate('authenticator:jwt', { password: googleToken} )
+          .then(callback(), function (error) {
+          console.log('FYB client >> custom token authentication failed!',
+            error.message);
+      });
+    }, function (error) {
+      console.error('FYB client >> Google auth failed: ', error.message);
     });
-    */
-  }, function (error) {
-    console.error('Google auth failed: ', error.message);
-  });
-}else if(provider.toLowerCase() === 'google' ){
-  routeContext.get('torii')
-  /**
-  * In the 'authorisation code grant' flow, the provider returns an authorisation
-  * code (once the pop-up and redirection steps have finished). The app is
-  * then responsible for translating that authorisation code into an actual
-  * access token.  This is done by the client app passing the authorisation
-  * code into the backend server, which will then use the authorisation code,
-  * along with various bits of configuration (app secrets, endpoints etc) to
-  * to get an access token from google's api.
-  */
-  .open('google-oauth2')
-  .then(authData => {
-    var prop = null;
-    for (prop in authData){
-      console.log('next key = ' + prop + '; value = ' + authData[prop]);
-    }
-    var authToken = authData.authorizationCode;  // auth code for later use
-    console.log('Google auth token: ' + authToken);
+  } else if (provider.toLowerCase() === 'google') {
+    console.log('<<< oauth2 authorization code grant flow >>>');
+    routeContext.get('torii')
+    /**
+     * In the 'authorisation code grant' flow, the provider returns an authorisation
+     * code (once authentication and redirection steps have finished).
+     * Our FYB server then can use that authorization code, to go back to the
+     * google apis - and fetch data from different google apps, for that end user.
+     */
+    .open('google-oauth2')
+      .then(authData => {
+        console.log('FYB client >> Authenticated with Google.');
+        var prop = null;
+        for (prop in authData){
+          console.log('' + prop + ' = ' + authData[prop]);
+        }
+        var authToken = authData.authorizationCode;
 
-    //  This bit is for interacting with the jwt provider - i.e. our server
-    routeContext.get('session')
-      .authenticate('authenticator:jwt', { identification: authToken} )
-    .then(callback(), function (error) {
-      console.log('custom token authentication failed!', error.message);
+        // Now hand the authorization code to our FYB server
+        routeContext.get('session')
+          .authenticate('authenticator:jwt', { identification: authToken} )
+        .then(callback(), function (error) {
+          console.log('custom token authentication failed!', error.message);
+        });
+      }, function (error) {
+        console.error('Google auth failed: ', error.message);
+      });
+  } else if(provider.toLowerCase() === 'facebook') {
+    console.log('facebook authentication');
+    var controller = this.controllerFor('users');
+    // The provider name is passed to `open`
+    this.get('torii').open('facebook-oauth2').then(function(authorization){
+      // FB.api is now available. authorization contains the UID and
+      // accessToken.
+      controller.set('hasFacebook', true);
     });
-  }, function (error) {
-    console.error('Google auth failed: ', error.message);
-  });
-}else if(provider.toLowerCase() === 'facebook'){
-  console.log('facebook authentication');
-  var controller = this.controllerFor('users');
-  // The provider name is passed to `open`
-  this.get('torii').open('facebook-oauth2').then(function(authorization){
-    // FB.api is now available. authorization contains the UID and
-    // accessToken.
-    controller.set('hasFacebook', true);
-  });
-}
+  }
 
 };
 
