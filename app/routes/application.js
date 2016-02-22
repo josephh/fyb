a54cccbc5915131c94182e9b2af28fc649ba2b56
josephh <josephh@rocketmail.com>
2016-02-22 10:45:32 +0000

    deleting unwanted routes, adding logout action

--- app/routes/application.js
+++ app/routes/application.js
@@ -15,8 +15,8 @@ export default Ember.Route.extend( {
     }
   },
   actions: {
-    invalidateSession: function(){
-      this.get('session').invalidate();
+    logout() {
+      this.get('session').close();
     }
   }
 });
